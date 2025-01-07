40073d2563267fe1c7ed0056014743697e1ad018
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-01-07 11:11:06 +0000

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
 Module to define a function that takes a key and a value,
-calculates the square of the value, and returns a tuple of the key and the squared value.
+calculates the square of the value, and returns a tuple of the key 
+and the squared value.
 
 This function ensures that the squared value is returned as a float.
 """
@@ -16,9 +17,11 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
 
     Args:
         k (str): The key to be paired with the squared value.
-        v (Union[int, float]): The value to be squared, which can be an integer or a float.
+        v (Union[int, float]): The value to be squared, which can be an integer 
+                                or a float.
 
     Returns:
-        Tuple[str, float]: A tuple containing the key and the squared value as a float.
+        Tuple[str, float]: A tuple containing the key and the squared value 
+                            as a float.
     """
     return k, float(v**2)
