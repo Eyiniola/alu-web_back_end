857e1bcdb8b46014b62ee4ab0b3662cc48dec009
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-01-07 09:14:26 +0000

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This function uses type annotations to specify what values the function arguments expect and what
+their out will be type 
+"""
+
+def add(a: float, b: float) -> float:
+    """"
+    This function produces the sum of:
+    Args a which is a float and 
+    Args b which is also a float
+    """
+
+    return a + b
