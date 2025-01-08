65fc89ed2d21cb8cd91f00b4665ba73f65c77398
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-01-08 17:13:22 +0000

    task

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -22,7 +22,8 @@ async def measure_runtime() -> float:
     """
     start_time = time.perf_counter()
 
-    await asyncio.gather(async_comprehension(),
+    await asyncio.gather(
+        async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
